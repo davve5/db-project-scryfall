7731e724a958bf3f43d7194face182bff2c0fc1b
Dominik-polar <elohimjad@gmail.com>
2024-05-17 21:21:14 +0200

    Update addCardToDeck

--- app/decks/addCardToDeck.py
+++ app/decks/addCardToDeck.py
@@ -7,9 +7,37 @@ cards = db["cards"]
 decks = db["decks"]
 
 
-foundCard = cards.find_one({ "name": 'Goblin Bowling Team' })
+print("Podaj nazwę talii: ")
 
-foundCardId = foundCard.get('_id')
+deckName = input()
 
-decks.insert_one({"deckName": "Test", "cardId": foundCardId, "userId": 1})
 
+while 1:
+    print("Podaj nazwę karty: ")
+    duplicate = False
+    cardName = input()
+
+    foundCard = cards.find_one({ "name": cardName })
+
+    foundDeck = decks.find_one({ "name": deckName, "userId": 1 })
+
+    if foundCard != None:
+
+        foundCardId = foundCard.get('_id')
+
+        if foundDeck == None:
+            decks.insert_one({"name": deckName, "cardsId": [foundCardId], "userId": 1})
+
+        else:
+            for card in foundDeck['cardsId']:
+                if card == foundCardId:
+                    duplicate = True
+            if duplicate:
+                    print("Podana karta jest już w talii")
+            else:
+                result = decks.update_one({'name': deckName, "userId": 1}, {'$push': { 'cardsId': {'$each': [foundCardId]}}})
+                print("Dodano kartę do talii")
+                print("Obecna ilość kart w talii wynosi:", len(foundDeck['cardsId']) + 1)
+    else:
+        print("Podana nazwa karty jest nieprawidłowa")
+        continue
