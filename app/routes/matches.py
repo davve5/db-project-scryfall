22db70c5e98ae2cbbbb1ff3572911735a9e5b67b
Dominik-polar <elohimjad@gmail.com>
2024-05-26 14:46:54 +0200

    Changed matches

--- app/routes/matches.py
+++ app/routes/matches.py
@@ -1,20 +1,44 @@
 from fastapi import APIRouter, status, HTTPException, Depends
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, ConfigDict
 from db.mongo import MongoManager
-from typing import Annotated
+from typing import Annotated, List
 from app.routes.auth import get_current_user, User
 from bson.objectid import ObjectId
 from db.neo4j import Neo4jManager
+from app.routes.cards_collection import create_user_card_relationship
+BaseModel.model_config["json_encoders"] = {ObjectId: lambda v: str(v)}
 
 class Match(BaseModel):
     my_deck_id: str
     enemy_id: str
     enemy_deck_id: str
     result: int
-		
+
 router = APIRouter()
 
+class MetchResult(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
+    id: ObjectId = Field(alias="_id")
+    winner_id: ObjectId = Field()
+    loser_id: ObjectId = Field()
+    winner_deck_id: ObjectId = Field()
+    looser_deck_id: ObjectId = Field()
+
+def delete_match_lost_won_relationship(winner_deck_id: str, looser_deck_id:str):
+    neo4j = Neo4jManager.get_instance()
+    params = { "winner_deck_id": str(winner_deck_id), "looser_deck_id": str(looser_deck_id) }
+    neo4j.run(
+
+        "MATCH (winner:Deck {id: $winner_deck_id})-[r:WON_AGAINST]->(looser:Deck {id: $looser_deck_id}) DELETE r",
+        params
+    )
+    neo4j.run(
+        "MATCH (looser:Deck {id: $looser_deck_id})-[r:LOST_AGAINST]->(winner:Deck {id: $winner_deck_id}) DELETE r",
+        params
+    )
+    
+    
 def create_match_lost_won_relationship(winner_deck_id: str, looser_deck_id:str):
     neo4j = Neo4jManager.get_instance()
     params = { "winner_deck_id": str(winner_deck_id), "looser_deck_id": str(looser_deck_id) }
@@ -27,11 +51,12 @@ def create_match_lost_won_relationship(winner_deck_id: str, looser_deck_id:str):
         params
     )
 
+
 @router.post("/")
 async def create(match: Match, current_user: Annotated[User, Depends(get_current_user)]):
     mongo = MongoManager.get_instance()
     enemy = mongo['users'].find_one({"_id": ObjectId(match.enemy_id)})
-    
+
     winner_id = current_user.id
     loser_id = enemy.get('_id')
 
@@ -40,28 +65,44 @@ async def create(match: Match, current_user: Annotated[User, Depends(get_current
 
     winner_deck = mongo['decks'].find_one({ "_id": ObjectId(match.my_deck_id) })
     looser_deck = mongo['decks'].find_one({ "_id": ObjectId(match.enemy_deck_id) })
-    
+
     match = mongo['matches'].insert_one({
         "winner_id": winner_id,
         "loser_id": loser_id,
         "winner_deck_id": winner_deck.get('_id'),
         "looser_deck_id": looser_deck.get('_id'),
-        "winner_deck": winner_deck.get('cards_id'),
-        "looser_deck": looser_deck.get('cards_id'),
     })
-    
+
     create_match_lost_won_relationship(winner_deck_id=winner_deck.get('_id'), looser_deck_id=looser_deck.get('_id'))
 
     return {"message": "Match created successfully", id: match.inserted_id}
 
-@router.get("/{match_id}", response_class=Match)
-async def find_one(match_id: str):
+@router.get("/{match_id}")
+async def find_one(match_id: str, current_user: Annotated[User, Depends(get_current_user)]):
     mongo = MongoManager.get_instance()
-    match = mongo['matches'].find_one({ "_id": ObjectId(match_id), "user_id": ObjectId(user_id) })
-    return match
+    match = mongo['matches'].find_one({ "_id": ObjectId(match_id), "$or": [
+            { "winner_id": ObjectId(current_user.id) },
+            { "loser_id": ObjectId(current_user.id) }
+        ]})
 
+    return MetchResult(**match)
+    
+    
 @router.delete("/{match_id}")
-async def delete(match_id: str):
+async def delete(match_id: str, current_user: Annotated[User, Depends(get_current_user)]):
     mongo = MongoManager.get_instance()
-    mongo['matches'].delete_one({ "_id": ObjectId(match_id), "user_id": ObjectId(user_id) })
+    match = mongo['matches'].find_one(
+        { "_id": ObjectId(match_id),
+        "$or": [
+            { "winner_id": ObjectId(current_user.id) },
+            { "loser_id": ObjectId(current_user.id) }
+        ]}
+        )
+    mongo['matches'].delete_one({ "_id": ObjectId(match_id),
+        "$or": [
+            { "winner_id": ObjectId(current_user.id) },
+            { "loser_id": ObjectId(current_user.id) }
+        ]})
+
+    delete_match_lost_won_relationship(winner_deck_id=match.get('winner_deck_id'), looser_deck_id=match.get('looser_deck_id'))
     return {"message": "Match deleted"}
--- seeders/matches.py
+++ seeders/matches.py
@@ -9,9 +9,17 @@ def insert():
     neo4j = Neo4jManager.get_instance()
     random.seed(1234)
     decks = mongo["decks"].find()
-    decks = list(map(lambda x: x.get('_id'), decks))
+    decks = list(decks)
 
-    for i in range(len(decks) * 2):
+    for i in range(len(decks) * 5):
         winner = random.choice(decks)
         looser = random.choice(decks)
-        create_match_lost_won_relationship(winner_deck_id=winner, looser_deck_id=looser)
+
+        match = mongo['matches'].insert_one({
+            "winner_id": winner.get('user_id'),
+            "loser_id": looser.get('user_id'),
+            "winner_deck_id": winner.get('_id'),
+            "looser_deck_id": looser.get('_id'),
+        })
+
+        create_match_lost_won_relationship(winner_deck_id=winner.get('_id'), looser_deck_id=looser.get('_id'))
